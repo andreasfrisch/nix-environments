8a245a828897f50c70cae434e6be2f8142c6c981
Andreas Frisch <andreas.frisch@gmail.com>
2025-07-31 14:23:56 +0200

    lockfiles added

--- python/flake.nix
+++ python/flake.nix
@@ -1,18 +1,32 @@
 {
   description = "Python devShell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-  outputs = { self, nixpkgs }: {
-    devShells = {
-      x86_64-linux = let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in {
-        default = pkgs.mkShell {
-          name = "python-shell";
-          buildInputs = with pkgs; [ python311 python311Packages.pip ];
-        };
-      };
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "python-shell";
+          buildInputs = with pkgs; [
+            python311
+            python311Packages.pip
+          ];
+
+          # Optional: set up virtualenv if needed
+          shellHook = ''
+            echo "Welcome to the Python 3.11 shell!"
+            if [ ! -d .venv ]; then
+              echo "Creating virtualenv in .venv..."
+              python3 -m venv .venv
+            fi
+            source .venv/bin/activate
+          '';
+        };
+      });
 }
