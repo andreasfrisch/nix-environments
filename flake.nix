499dee5db476030f74d8e62db8149a38f3a349b2
Andreas Frisch <andreas.frisch@gmail.com>
2025-07-31 14:54:31 +0200

    let's try templates instead

--- flake.nix
+++ flake.nix
@@ -1,15 +1,171 @@
 {
-  description = "Flake delegating to dev shells in subfolders";
+  description = "Ready-made templates for easily creating flake-driven environments";
 
-  inputs.python.url = "github:andreasfrisch/nix-environments?dir=python";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = { self, python, ... }: {
-    devShells = {
-      x86_64-linux = {
-        python = python.devShells.x86_64-linux.default;
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+
+      scriptDrvs = forEachSupportedSystem ({ pkgs }:
+        let
+          getSystem = "SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')";
+          forEachDir = exec: ''
+            for dir in */; do
+              (
+                cd "''${dir}"
+
+                ${exec}
+              )
+            done
+          '';
+        in
+        {
+          format = pkgs.writeShellApplication {
+            name = "format";
+            runtimeInputs = with pkgs; [ nixpkgs-fmt ];
+            text = ''
+              shopt -s globstar
+
+              nixpkgs-fmt -- **/*.nix
+            '';
+          };
+
+          # only run this locally, as Actions will run out of disk space
+          build = pkgs.writeShellApplication {
+            name = "build";
+            text = ''
+              ${getSystem}
+
+              ${forEachDir ''
+                echo "building ''${dir}"
+                nix build ".#devShells.''${SYSTEM}.default"
+              ''}
+            '';
+          };
+
+          check = pkgs.writeShellApplication {
+            name = "check";
+            text = forEachDir ''
+              echo "checking ''${dir}"
+              nix flake check --all-systems --no-build
+            '';
+          };
+
+          update = pkgs.writeShellApplication {
+            name = "update";
+            text = forEachDir ''
+              echo "updating ''${dir}"
+              nix flake update
+            '';
+          };
+        });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages =
+            with scriptDrvs.${pkgs.system}; [
+              build
+              check
+              format
+              update
+            ] ++ [ pkgs.nixpkgs-fmt ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }:
+        rec {
+          default = dvt;
+          dvt = pkgs.writeShellApplication {
+            name = "dvt";
+            bashOptions = [ "errexit" "pipefail" ];
+            text = ''
+              if [ -z "''${1}" ]; then
+                echo "no template specified"
+                exit 1
+              fi
+
+              TEMPLATE=$1
+
+              nix \
+                --experimental-features 'nix-command flakes' \
+                flake init \
+                --template \
+                "https://flakehub.com/f/the-nix-way/dev-templates/0.1#''${TEMPLATE}"
+            '';
+          };
+        }
+      );
+    }
+
+    //
+
+    {
+      templates = rec {
+        default = empty;
+
+        #c-cpp = {
+        #  path = ./c-cpp;
+        #  description = "C/C++ development environment";
+        #};
+
+        empty = {
+          path = ./empty;
+          description = "Empty dev template that you can customize at will";
+        };
+
+        #go = {
+        #  path = ./go;
+        #  description = "Go (Golang) development environment";
+        #};
+
+        #haskell = {
+        #  path = ./haskell;
+        #  description = "Haskell development environment";
+        #};
+
+        #latex = {
+        #  path = ./latex;
+        #  description = "LaTeX development environment";
+        #};
+
+        #nix = {
+        #  path = ./nix;
+        #  description = "Nix development environment";
+        #};
+
+        #node = {
+        #  path = ./node;
+        #  description = "Node.js development environment";
+        #};
+
+        #protobuf = {
+        #  path = ./protobuf;
+        #  description = "Protobuf development environment";
+        #};
+
+        python = {
+          path = ./python;
+          description = "Python development environment";
+        };
+
+        #rust = {
+        #  path = ./rust;
+        #  description = "Rust development environment";
+        #};
+
+        #shell = {
+        #  path = ./shell;
+        #  description = "Shell script development environment";
+        #};
+
+        # Aliases
+        #c = c-cpp;
+        #cpp = c-cpp;
       };
     };
-
-    python = python.devShells.x86_64-linux.default;
-  };
 }
--- python/flake.nix
+++ python/flake.nix
@@ -1,32 +1,79 @@
 {
-  description = "Python devShell";
+  description = "A Nix-flake-based Python development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        devShells.default = pkgs.mkShell {
-          name = "python-shell";
-          buildInputs = with pkgs; [
-            python311
-            python311Packages.pip
-          ];
-
-          # Optional: set up virtualenv if needed
-          shellHook = ''
-            echo "Welcome to the Python 3.11 shell!"
-            if [ ! -d .venv ]; then
-              echo "Creating virtualenv in .venv..."
-              python3 -m venv .venv
-            fi
-            source .venv/bin/activate
-          '';
-        };
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
       });
+
+      /*
+       * Change this value ({major}.{min}) to
+       * update the Python virtual-environment
+       * version. When you do this, make sure
+       * to delete the `.venv` directory to
+       * have the hook rebuild it for the new
+       * version, since it won't overwrite an
+       * existing one. After this, reload the
+       * development shell to rebuild it.
+       * You'll see a warning asking you to
+       * do this when version mismatches are
+       * present. For safety, removal should
+       * be a manual step, even if trivial.
+       */
+      version = "3.13";
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }:
+        let
+          concatMajorMinor = v:
+            pkgs.lib.pipe v [
+              pkgs.lib.versions.splitVersion
+              (pkgs.lib.sublist 0 2)
+              pkgs.lib.concatStrings
+            ];
+
+          python = pkgs."python${concatMajorMinor version}";
+        in
+        {
+          default = pkgs.mkShell {
+            venvDir = ".venv";
+
+            postShellHook = ''
+              venvVersionWarn() {
+              	local venvVersion
+              	venvVersion="$("$venvDir/bin/python" -c 'import platform; print(platform.python_version())')"
+
+              	[[ "$venvVersion" == "${python.version}" ]] && return
+
+              	cat <<EOF
+              Warning: Python version mismatch: [$venvVersion (venv)] != [${python.version}]
+                       Delete '$venvDir' and reload to rebuild for version ${python.version}
+              EOF
+              }
+
+              venvVersionWarn
+            '';
+
+            packages = with python.pkgs; [
+              venvShellHook
+              pip
+
+              /* Add whatever else you'd like here. */
+              # pkgs.basedpyright
+
+              # pkgs.black
+              /* or */
+              # python.pkgs.black
+
+              # pkgs.ruff
+              /* or */
+              # python.pkgs.ruff
+            ];
+          };
+        });
+    };
 }
