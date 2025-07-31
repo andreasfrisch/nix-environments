f91e51fb01070d6a96efae732acf65a10039032a
Andreas Frisch <andreas.frisch@gmail.com>
2025-07-31 09:38:28 +0200

    python flake with architecture

--- python/flake.nix
+++ python/flake.nix
@@ -4,13 +4,15 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
   outputs = { self, nixpkgs }: {
-    devShells.default = let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
+    devShells = {
+      x86_64-linux = let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in {
+        default = pkgs.mkShell {
+          name = "python-shell";
+          buildInputs = with pkgs; [ python311 python311Packages.pip ];
+        };
       };
-    in pkgs.mkShell {
-      name = "python-env";
-      buildInputs = with pkgs; [ python311 python311Packages.pip ];
     };
   };
 }
